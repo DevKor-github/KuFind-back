b7e92ee31b095920bd37dec5c7bc840c77f8527e
kimh7537 <kimh7537@gmail.com>
2023-11-26 19:07:00 +0900

    메인 페이지 생성

--- KuFind_back/urls.py
+++ KuFind_back/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for klub_back project.
+URL configuration for KuFind_back project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
